ea88c558ac20e12b8706b93226e4dc2aa9179e79
Tarikuloashi <tarikuloashi@gmail.com>
2018-01-11 12:11:46 +0600

    Make Register with sentinel controller

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register','RegistrationController@register');
+
+Route::post('/register','RegistrationController@postregister');
